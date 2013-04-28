cfe027eb60153e0bc5596de2a4e2184cff3e5b05
whostolemyhat <jamestease@gmail.com>
2013-04-28 11:07:52 +0100

    Changed to dynamic rotation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,164 +12,186 @@
 */
 (function(tessellate, $, undefined) {
     // var == private
-    var S_SHAPE_TEMPLATE = [['.....',
-                         '.....',
-                         '..00.',
-                         '.00..',
-                         '.....'],
-                        ['.....',
-                         '..0..',
-                         '..00.',
-                         '...0..',
-                         '.....']];
+    var shapes = [
+        [[0, 1, 0, 0],
+         [0, 1, 0, 0],
+         [0, 1, 0, 0],
+         [0, 1, 0, 0]],
 
-    var Z_SHAPE_TEMPLATE = [['.....',
-                         '.....',
-                         '.00..',
-                         '..00.',
-                         '.....'],
-                        ['.....',
-                         '..0..',
-                         '.00..',
-                         '.0...',
-                         '.....']];
+        [[0, 0, 0, 0],
+         [0, 1, 1, 0],
+         [0, 1, 1, 0],
+         [0, 0, 0, 0]],
 
-    var I_SHAPE_TEMPLATE = [['..0..',
-                         '..0..',
-                         '..0..',
-                         '..0..',
-                         '.....'],
-                        ['.....',
-                         '.....',
-                         '0000.',
-                         '.....',
-                         '.....']];
+        [[0, 0, 0, 0],
+         [0, 1, 0, 0],
+         [1, 1, 1, 0],
+         [0, 0, 0, 0]],
 
-    var O_SHAPE_TEMPLATE = [['.....',
-                         '.....',
-                         '.00..',
-                         '.00..',
-                         '.....']];
+        [[0, 0, 0, 0],
+         [0, 0, 1, 0],
+         [0, 0, 1, 0],
+         [0, 1, 1, 0]],
 
-    var J_SHAPE_TEMPLATE = [['.....',
-                         '.0...',
-                         '.000.',
-                         '.....',
-                         '.....'],
-                        ['.....',
-                         '..00.',
-                         '..0..',
-                         '..0..',
-                         '.....'],
-                        ['.....',
-                         '.....',
-                         '.000.',
-                         '...0.',
-                         '.....'],
-                        ['.....',
-                         '..0..',
-                         '..0..',
-                         '.00..',
-                         '.....']];
+        [[0, 0, 0, 0],
+         [0, 1, 0, 0],
+         [0, 1, 0, 0],
+         [0, 1, 1, 0]],
 
-    var L_SHAPE_TEMPLATE = [['.....',
-                         '...0.',
-                         '.000.',
-                         '.....',
-                         '.....'],
-                        ['.....',
-                         '..0..',
-                         '..0..',
-                         '..00.',
-                         '.....'],
-                        ['.....',
-                         '.....',
-                         '.000.',
-                         '.0...',
-                         '.....'],
-                        ['.....',
-                         '.00..',
-                         '..0..',
-                         '..0..',
-                         '.....']];
+        [[0, 0, 0, 0],
+         [0, 0, 1, 0],
+         [0, 1, 1, 0],
+         [0, 1, 0, 0]],
 
-    var T_SHAPE_TEMPLATE = [['.....',
-                         '..0..',
-                         '.000.',
-                         '.....',
-                         '.....'],
-                        ['.....',
-                         '..0..',
-                         '..00.',
-                         '..0..',
-                         '.....'],
-                        ['.....',
-                         '.....',
-                         '.000.',
-                         '..0..'],
-                        ['.....',
-                         '..0..',
-                         '.00..',
-                         '..0..',
-                         '.....']];
+        [[0, 0, 0, 0],
+         [0, 1, 0, 0],
+         [0, 1, 1, 0],
+         [0, 0, 1, 0]]
+    ];
 
-    // tessellate == public
-    tessellate.SHAPES = {
-        S: S_SHAPE_TEMPLATE,
-        Z: Z_SHAPE_TEMPLATE,
-        J: J_SHAPE_TEMPLATE,
-        L: L_SHAPE_TEMPLATE,
-        T: T_SHAPE_TEMPLATE
-    };
+    function Shape(S) {
+
+        S = S || {};
+
+        S.rotate = function() {
+            console.log('rotating');
+
+            var rotatedShape = [
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ];
+            
+            var shapeLength = 4;
+            // http://charlesleifer.com/blog/even-more-canvas-fun-tetris-in-javascript/
+            for(var y = 0; y < shapeLength; y++) {
+                for(var x = 0; x < shapeLength; x++) {
+                    rotatedShape[x][y] = S.shape[shapeLength - y - 1][x];
+                }
+            }
+            S.shape = rotatedShape;
+        };
+
+        S.leftEdge = function() {};
+        S.rightEdge = function() {};
+        S.bottomEdge = function() {};
+        S.init = function() {
+            var rotation = Math.floor(Math.random() * 4);
+            var randShape = Math.floor(Math.random() * shapes.length);
+            S.shape = shapes[randShape];
+            for(var i = 0; i < rotation; i++) {
+                S.rotate();
+            }
+        };
+        S.clone = function() {};
+        S.draw = function() {
+            for(var y = 0; y < S.shape.length; y++) {
+                var row = S.shape[y];
+                for(var x = 0; x < row.length; x++) {
+                    drawBlock(x, y, row[x]);
+                }
+            }
+        };
+
+        return S;
+    }
+
+    var Board = {
+        colour: '#ccc',
+        x: 0,
+        y: 0,
+        height: 20,
+        width: 12,
+
+        board: [],
+
+        init: function() {
+            for(var y = 0; y < this.height; y++) {
+                var row = [];
+                for(var x = 0; x < this.width; x++) {
+                    row.push(0);
+                }
+                this.board.push(row);
+            }
+        },
+
+        update: function() {},
+        draw: function() {
+            // rect(this.x, this.y, this.width, this.height, this.colour);
+            // iterate through board
+            for(var y = 0; y < this.board.length; y++) {
+                var row = this.board[y];
+                for(var x = 0; x < row.length; x++) {
+                    drawBlock(x, y, row[x]);    
+                }
+            }
+        }
+    }
+
+
+    function drawBlock(x, y, blockType) {
+        var colour;
+        switch(blockType) {
+            case 0:
+                colour = Board.colour;
+                break;
+            default:
+                colour = 'red';
+                break;
+        }
+
+        x = x * TILE_SIZE;
+        y = y * TILE_SIZE;
+        rect(x, y, TILE_SIZE, TILE_SIZE, colour);
+    }
 
     var play;
     var fallingPiece;
     var nextPiece;
-    var CANVAS_WIDTH = 170;
-    var CANVAS_HEIGHT = 510;
-    var TEMPLATE_WIDTH = 5;
-    var TILE_WIDTH = 15;
-    var TILE_HEIGHT = 15;
-    var BORDER_WIDTH = 2;
-    var MOVE_DOWN_FREQ = 100;
-    var MOVE_SIDEWAYS_FREQ = 150;
+    var CANVAS_WIDTH;
+    var CANVAS_HEIGHT;
+    var TILE_SIZE = 20;
+    // var TILE_HEIGHT = 15;
+    // var BORDER_WIDTH = 2;
+    // var MOVE_DOWN_FREQ = 100;
+    // var MOVE_SIDEWAYS_FREQ = 150;
 
-    $('<canvas id="canvas" width="' + CANVAS_WIDTH + '" height="' + CANVAS_HEIGHT + '"></canvas>').appendTo('.container');
+    // $('<canvas id="canvas" width="' + CANVAS_WIDTH + '" height="' + CANVAS_HEIGHT + '"></canvas>').appendTo('.container');
     var canvas;
     var ctx;
 
-    var board = {
-        width: 10,
-        height: 30,
-        draw: function() {
-            for(var x = 0; x < this.width; x++) {
-                for(var y = 0; y < this.height; y++) {
-                    rect((x * TILE_WIDTH) + (x * BORDER_WIDTH), (y * TILE_HEIGHT) + (y * BORDER_WIDTH), TILE_WIDTH, TILE_HEIGHT, '#2e2');
-                }
-            }
-        },
-        pieces: []
-    };
+    // var board = {
+    //     width: 10,
+    //     height: 30,
+    //     draw: function() {
+    //         for(var x = 0; x < this.width; x++) {
+    //             for(var y = 0; y < this.height; y++) {
+    //                 rect((x * TILE_WIDTH) + (x * BORDER_WIDTH), (y * TILE_HEIGHT) + (y * BORDER_WIDTH), TILE_WIDTH, TILE_HEIGHT, '#2e2');
+    //             }
+    //         }
+    //     },
+    //     pieces: []
+    // };
 
-    var piece = {
-        x: 0,
-        y: 0,
-        draw: function() {
-
-        }
-    };
-
-    tessellate.getPieces = function() {
-        return board.pieces;
-    }
 
     tessellate.init = function() {
-        canvas = canvas || $('#canvas')[0];
-        ctx = ctx || canvas.getContext('2d');
+        canvas = $('#canvas')[0];
+        ctx = canvas.getContext('2d');
+
+        CANVAS_WIDTH = canvas.width;
+        CANVAS_HEIGHT = canvas.height;
         ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-        fallingPiece = getNewPiece();
-        nextPiece = getNewPiece();
+        fallingPiece = new Shape();
+        nextPiece = new Shape();
+
+        fallingPiece.init();
+        nextPiece.init();
+
+        Board.init();
+
+        console.log(Board.board);
 
         // debug
         // board.pieces = [];
@@ -184,27 +206,15 @@
     };
 
     var draw = function() {
-        console.log('drawing');
+        // console.log('drawing');
         ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        board.draw();
-        for(var i = 0; i < board.pieces.length; i++) {
-            drawPiece(board.pieces[i]);
-        }
-        drawPiece(fallingPiece);
+        Board.draw();
+        fallingPiece.draw();
     };
 
     var update = function() {
 
-        if(!fallingPiece) {
-            fallingPiece = nextPiece;
-            nextPiece = getNewPiece();
-        }
 
-        fallingPiece.y += 1;
-
-        if(!validMove(board, fallingPiece)) {
-            gameOver();
-        }
     };
 
     tessellate.end = function() {
@@ -212,52 +222,10 @@
         console.log('stopped');
     };
 
-    var getNewPiece = function() {
-        /** 
-        * Returns random shape in random rotation
-        */
-        var keys = Object.keys(tessellate.SHAPES);
-        var shape = keys[Math.floor(keys.length * Math.random())];
-        var newPiece = {
-            shape: shape,
-            rotation: tessellate.SHAPES[shape][Math.floor(tessellate.SHAPES[shape].length * Math.random())],
-            x: 0,
-            y: 0, // start above board
-            colour: 'red'
-        };
 
-        return newPiece;
-    };
-    
-    var gameOver = function() {
-        console.log('game over');
-    }
-
-    var validMove = function(board, fallingPiece) {
-        return true;
-    }
-
-    var drawPiece = function(piece) {
-        console.log(piece);
-        var arr = piece.rotation
-        for(var y = 0; y < piece.rotation.length; y++) {
-            var arr = piece.rotation[y].split('');
-            for(var x = 0; x < arr.length; x++) {
-                if(arr[x] === '0') {
-                    rect(
-                        ((x + piece.x) * TILE_WIDTH) + (x * BORDER_WIDTH), 
-                        ((y + piece.y) * TILE_HEIGHT) + (y * BORDER_WIDTH), 
-                        TILE_WIDTH, 
-                        TILE_HEIGHT, 
-                        piece.colour
-                        );
-                }
-            }
-        }
-    }
 
     // TODO: move to lib
-    function rect(x, y, height, width, colour) {
+    function rect(x, y, width, height, colour) {
         ctx.fillStyle = colour;
         ctx.beginPath();
         ctx.fillRect(x, y, width, height);
@@ -267,7 +235,15 @@
         // ctx.strokeStyle = '#2e2';
         // ctx.stroke();
         ctx.closePath();
-    }
+    };
+
+    tessellate.handleKeys = function(e) {
+        switch(e.keyCode) {
+            case 38:
+                fallingPiece.rotate();
+                break;
+        }
+    };
 
 })(window.tessellate = window.tessellate || {}, jQuery);
 
@@ -275,6 +251,7 @@
 $(document).ready(function() {
     if(Modernizr.canvas) {
 
+        tessellate.init();
 
         $('#start').click(function() {
             tessellate.init();
@@ -283,6 +260,10 @@ $(document).ready(function() {
         $('#stop').click(function() {
             tessellate.end();
             return false;
+        });
+
+        $(document).keydown(function(e) {
+            tessellate.handleKeys(e);
         });
 
     } else {
